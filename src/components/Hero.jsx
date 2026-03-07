cc485a4ad81b31257e15c1b720bea707835ce02f
Ayush Anand <ayushanandhd@gmail.com>
2026-03-07 12:05:06 +0530

    upgrade gemini model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import './Hero.css'
 import Song from "./Song";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
 
 export default function Hero() {
